521d3a617585f034d60daa6bfb0580c4fd515931
SzklevanMilan <szklevanm@gmail.com>
2020-09-15 09:59:44 +0200

    Add project files.

---
+++ órai munka0915/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace órai_munka0915
+{
+    class Teglalap
+    {
+        public int a, b;
+        public int Kerulet()
+        {
+            return 2 * (a + b);
+        }
+        public int Terulet()
+        {
+            return a * b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teglalap oldal = new Teglalap();
+            oldal.a = 15;
+            oldal.b = 10;
+            Console.WriteLine("A téglalap kerülete {0}.",oldal.Kerulet());
+            Console.WriteLine("A téglalap területe {0}.",oldal.Terulet());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
